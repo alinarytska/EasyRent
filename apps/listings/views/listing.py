bde1292b92a11e2ccd7520a3aba77c04ca6d8646
Alina_Rytska <alinarytskade@gmail.com>
2026-07-12 04:07:04 +0200

    add my listings endpoint

--- apps/listings/views/listing.py
+++ apps/listings/views/listing.py
@@ -1,5 +1,7 @@
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.decorators import action
 from rest_framework.filters import OrderingFilter, SearchFilter
+from rest_framework.response import Response
 from rest_framework import viewsets
 
 from apps.listings.filters import ListingFilter
@@ -34,3 +36,18 @@ class ListingViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
+
+    @action(detail=False, methods=("get",), url_path="my")
+    def my_listings(self, request):
+        queryset = self.filter_queryset(
+            self.get_queryset().by_owner(request.user),
+        )
+        page = self.paginate_queryset(queryset)
+
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+
+        return Response(serializer.data)
