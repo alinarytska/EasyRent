a0bbe8c51f402726d06994fdcf57369b44291f43
Alina_Rytska <alinarytskade@gmail.com>
2026-07-12 03:53:20 +0200

    add listing search

--- apps/listings/views/listing.py
+++ apps/listings/views/listing.py
@@ -1,3 +1,5 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
 from rest_framework import viewsets
 
 from apps.listings.filters import ListingFilter
@@ -7,7 +9,15 @@ from apps.listings.serializers import ListingSerializer
 
 class ListingViewSet(viewsets.ModelViewSet):
     serializer_class = ListingSerializer
+    filter_backends = (DjangoFilterBackend, SearchFilter)
     filterset_class = ListingFilter
+    search_fields = (
+        "title",
+        "description",
+        "city",
+        "district",
+        "street",
+    )
 
     def get_queryset(self):
         return (
