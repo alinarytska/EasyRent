c59e3187ce345e4f8a21f7c5259a2d96f2591d68
Alina_Rytska <alinarytskade@gmail.com>
2026-07-13 04:13:22 +0200

    add popular listings by views endpoint

--- apps/view_history/views/view_history.py
+++ apps/view_history/views/view_history.py
@@ -1,6 +1,11 @@
+from django.db.models import Count
+from drf_spectacular.utils import extend_schema
 from rest_framework import status, viewsets
+from rest_framework.decorators import action
 from rest_framework.response import Response
 
+from apps.listings.models import Listing
+from apps.listings.serializers import ListingSerializer
 from apps.view_history.models import ViewHistory
 from apps.view_history.serializers import ViewHistorySerializer
 
@@ -15,6 +20,26 @@ class ViewHistoryViewSet(viewsets.ModelViewSet):
             return queryset.none()
 
         return queryset.filter(user=self.request.user)
+
+    @extend_schema(responses=ListingSerializer(many=True))
+    @action(detail=False, methods=("get",), url_path="popular-listings")
+    def popular_listings(self, request):
+        queryset = (
+            Listing.objects.active()
+            .select_related("owner")
+            .prefetch_related("images")
+            .annotate(views_count=Count("view_history"))
+            .filter(views_count__gt=0)
+            .order_by("-views_count", "-created_at")
+        )
+        page = self.paginate_queryset(queryset)
+
+        if page is not None:
+            serializer = ListingSerializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = ListingSerializer(queryset, many=True)
+        return Response(serializer.data)
 
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
