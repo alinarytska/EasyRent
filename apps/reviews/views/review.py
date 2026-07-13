f42adbc8364ba6301d18cc2e9cf715cb6d0e6450
Alina_Rytska <alinarytskade@gmail.com>
2026-07-13 04:04:42 +0200

    add my reviews endpoint

--- apps/reviews/views/review.py
+++ apps/reviews/views/review.py
@@ -1,7 +1,9 @@
 from django.db.models import Q
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
+from rest_framework.decorators import action
 from rest_framework.filters import OrderingFilter
+from rest_framework.response import Response
 
 from apps.reviews.filters import ReviewFilter
 from apps.reviews.models import Review
@@ -37,3 +39,17 @@ class ReviewViewSet(viewsets.ModelViewSet):
         return queryset.filter(
             Q(booking__renter=user) | Q(booking__listing__owner=user),
         ).distinct()
+
+    @action(detail=False, methods=("get",), url_path="my")
+    def my_reviews(self, request):
+        queryset = self.filter_queryset(
+            self.get_queryset().filter(booking__renter=request.user),
+        )
+        page = self.paginate_queryset(queryset)
+
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
