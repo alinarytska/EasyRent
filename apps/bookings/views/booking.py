1103e69751380173092ab74503c6f2184aa57ff7
Alina_Rytska <alinarytskade@gmail.com>
2026-07-13 03:46:49 +0200

    add my bookings endpoint

--- apps/bookings/views/booking.py
+++ apps/bookings/views/booking.py
@@ -1,8 +1,10 @@
 from django.db import transaction
 from django.db.models import Q
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.filters import OrderingFilter
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.filters import OrderingFilter
+from rest_framework.response import Response
 
 from apps.bookings.filters import BookingFilter
 from apps.bookings.models import Booking
@@ -39,6 +41,20 @@ class BookingViewSet(viewsets.ModelViewSet):
         return queryset.filter(
             Q(renter=user) | Q(listing__owner=user),
         ).distinct()
+
+    @action(detail=False, methods=("get",), url_path="my")
+    def my_bookings(self, request):
+        queryset = self.filter_queryset(
+            self.get_queryset().for_renter(request.user),
+        )
+        page = self.paginate_queryset(queryset)
+
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
 
     @transaction.atomic
     def perform_create(self, serializer):
